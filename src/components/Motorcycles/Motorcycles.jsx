1f2703792e273f552e70ca9d8d5619038f4cfc2f
Poleac Liviu Stefan <stefan.liviu28^2yahoo.ro>
2023-01-27 22:28:34 +0200

    current

---
+++ src/components/Motorcycles/Motorcycles.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Motorcycles = () => {
+  return (
+    <div className='relative mt-[5rem] bg-[#F8F8F8] w-full h-[40rem]'>
+
+    </div>
+  )
+}
+
+export default Motorcycles
