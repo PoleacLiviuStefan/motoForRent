e204eb05d93782588418265195c6111e3a112fc9
Poleac Liviu Stefan <stefan.liviu28^2yahoo.ro>
2023-01-31 00:27:31 +0200

    motorcycles

--- src/components/Motorcycles/Motorcycles.jsx
+++ src/components/Motorcycles/Motorcycles.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import MotorcycleCard from './MotorcycleCard'
+import yamaha from './Images/yamaha_mt7.png'
+import kawasaki from "./Images/kawasakiversys.png"
 const Motorcycles = () => {
   return (
-    <div className='relative mt-[5rem] bg-[#F8F8F8] w-full h-[40rem]'>
-
+    <div name="Motorcycles" className='relative flex flex-col items-center   mt-[5rem] bg-white w-full h-[80rem] lg:h-[40rem]'>
+        <div className='flex flex-col items-center lg:items-left lg:flew-row   lg:flex-row lg:w-[80%] w-full h-full'>
+            <MotorcycleCard image={yamaha} name={<>YAMAHA <span className='text-[#F9844A]'> MT7 </span></>} pret={<><span className='font-bold'> 40-65</span></>} an="2018" cp="80" rezervor="14" cmc="690"  />
+            <MotorcycleCard  image={kawasa} name={<>KAWASAKI<span className='text-[#F9844A]'> VERSYS </span></>} pret={<><span className='font-bold'> 40-65</span></>} an="2012" cp="64" rezervor="19" cmc="650"  />
+        </div>
     </div>
   )
 }
---
+++ src/components/Tururi/Tururi.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Tururi = () => {
+  return (
+    <div>
+        <h1 className='text-[36px] lg:text-[64px]'>TURURUI GHIDATE</h1>
+        
+    </div>
+  )
+}
+
+export default Tururi
